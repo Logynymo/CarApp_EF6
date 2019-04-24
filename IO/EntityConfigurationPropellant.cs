1aa9361b7e6c9a4f7045cbf2b18b161da39767e2
Donzulo <mads5376@edu.campusvejle.dk>
2019-04-24 09:18:37 +0200

    EntityConfigurationPropellant finished

---
+++ IO/EntityConfigurationPropellant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using REPO;
+
+namespace IO
+{
+    public class EntityConfigurationPropellant : EntityTypeConfiguration<Propellant>
+    {
+        public EntityConfigurationPropellant()
+        {
+            this.ToTable("Propellants");
+
+            this.HasKey<int>(p => p.PropellantId);
+
+            this.Property(p => p.PropellantName)
+                .HasMaxLength(20)
+                .IsRequired();
+        }
+    }
+}
