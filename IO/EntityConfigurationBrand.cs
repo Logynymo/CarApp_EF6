7cb49f65beb4f31ae41db40e4d528bda8ced53a5
Donzulo <mads5376@edu.campusvejle.dk>
2019-04-24 09:15:57 +0200

    EntityConfigurationBrand finished

---
+++ IO/EntityConfigurationBrand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using REPO;
+
+namespace IO
+{
+    public class EntityConfigurationBrand : EntityTypeConfiguration<Brand>
+    {
+        public EntityConfigurationBrand()
+        {
+            this.ToTable("Brands");
+
+            this.HasKey<int>(b => b.BrandId);
+
+            this.Property(b => b.BrandName)
+                .HasMaxLength(20)
+                .IsRequired();
+        }
+    }
+}
